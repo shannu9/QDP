a4b51a8fa708c1ea29a826c7ab8fe101ef2515b1
shannu9 <64985110+shannu9@users.noreply.github.com>
2025-04-08 16:09:54 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-apt-get update
-apt-get install -y tesseract-ocr poppler-utils
-# Export path to Tesseract binary for pytesseract
-export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
-export PATH="$PATH:/usr/bin"
-streamlit run quickdraw_app.py --server.port $PORT --server.address 0.0.0.0
+
+# Update and install tesseract
+apt-get update && apt-get install -y tesseract-ocr poppler-utils
+
+# Let Python know where tesseract is
+echo 'pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"' >> tesseract_path.py
+
+# Run the app with this path included
+echo "from tesseract_path import *" > wrapper.py
+cat quickdraw_app.py >> wrapper.py
+
+# Run the Streamlit app
+streamlit run wrapper.py --server.port $PORT --server.address 0.0.0.0
