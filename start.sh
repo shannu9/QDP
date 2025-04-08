cd95b0f23438e98935b7f987773803e06dd083b2
shannu9 <64985110+shannu9@users.noreply.github.com>
2025-04-08 16:15:16 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Update and install tesseract
+# Install system packages
 apt-get update && apt-get install -y tesseract-ocr poppler-utils
 
-# Let Python know where tesseract is
+# Create a file that defines the tesseract path
+echo 'import pytesseract' > tesseract_path.py
 echo 'pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"' >> tesseract_path.py
 
-# Run the app with this path included
+# Prepend that into the main app so it always loads first
 echo "from tesseract_path import *" > wrapper.py
 cat quickdraw_app.py >> wrapper.py
 
