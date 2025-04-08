7d3bfea0a3c6a96126edfb105031139a4c5c4189
shannu9 <64985110+shannu9@users.noreply.github.com>
2025-04-08 15:59:28 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y tesseract-ocr poppler-utils
+# Export path to Tesseract binary for pytesseract
+export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
+export PATH="$PATH:/usr/bin"
 streamlit run quickdraw_app.py --server.port $PORT --server.address 0.0.0.0
